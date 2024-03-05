869aa8798e78454dc307433d843fe426ece0e41c
Feat <123049841+Feat3719@users.noreply.github.com>
2024-03-05 12:42:20 +0900

    Title: 다양하게 출력, Time: 93ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240305/다양하게 출력/print-in-variety.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        System.out.println("Total days in Year\n365\nCircumference rate");
+        System.out.print(new java.math.BigDecimal(String.valueOf(Math.PI)).setScale(10, java.math.RoundingMode.DOWN).toPlainString());
+    }
+}
