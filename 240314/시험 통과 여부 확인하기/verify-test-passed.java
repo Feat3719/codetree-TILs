a3ce2142608efed20f28b8311f15b7cf4e7c0c52
Feat <123049841+Feat3719@users.noreply.github.com>
2024-03-14 18:33:14 +0900

    Title: 시험 통과 여부 확인하기, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 240314/시험 통과 여부 확인하기/verify-test-passed.java
+++ 240314/시험 통과 여부 확인하기/verify-test-passed.java
@@ -7,7 +7,7 @@ public class Main {
         if (n >= 80 ) {
             System.out.print("pass");
         } else {
-            System.out.print((100-n)+ " more score");
+            System.out.print((80-n)+ " more score");
         }
 
         sc.close();
